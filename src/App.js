ff6fb8985e7160b85886ab6e506dcfc942f6e1fe
Artem Smirnov <aratatem2013@gmail>
2021-05-29 14:57:06 +0300

    made a copy of HW 2

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './Components/ContactForm';
+import ContactList from './Components/ContactList';
+import Filter from './Components/Filter';
+import Container from './Components/Container';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const normalizedName = name.toLocaleLowerCase();
+    const contact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+
+    contacts.some(
+      contact => contact.name.toLocaleLowerCase() === normalizedName,
+    )
+      ? alert(`${name} is already in contacts`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList contacts={visibleContacts} onDelete={this.deleteContact} />
+      </Container>
+    );
+  }
+}
+
+export default App;
